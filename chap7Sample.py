b69ed3407226aa181b558fdf5bf009a839e21c46
Brian Donovan <bdonovan0726@gmail.com>
2026-03-23 18:53:44 -0500

    added some sample dictionary usage

---
+++ chap7Sample.py
@@ -0,0 +1,14 @@
+#sazmple file demonstrating some dictionary usage
+
+nameStr = 'Brian T. Donovan'
+charCount = {}
+
+for char in nameStr:
+	charCount.setdefault(char, 0)
+	charCount[char] += 1
+	
+for i in charCount.items():#dumps the indexes and values
+	print(i)
+    
+for x in charCount.keys():
+    print(x + ': ' + str(charCount[x]))
